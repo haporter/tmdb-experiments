02a2b2410d518bbd28edc021a73ecbc82159efb5
Andrew Porter <haporter@me.com>
2017-05-14 17:35:59 -0600

    MovieDetailVC wireframe rough draft

--- tmdb-experiements/tmdb-experiements/MovieDetailViewController.swift
+++ tmdb-experiements/tmdb-experiements/MovieDetailViewController.swift
@@ -10,7 +10,16 @@ import UIKit
 
 class MovieDetailViewController: UIViewController {
     
-    
+    @IBOutlet weak var bannerImageView: UIImageView!
+    @IBOutlet weak var posterImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var yearRatingLabel: UILabel!
+    @IBOutlet weak var runTimeLabel: UILabel!
+    @IBOutlet weak var userRatingLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var genreLabel: UILabel!
+    @IBOutlet weak var directorLabel: UILabel!
+    @IBOutlet weak var castLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
