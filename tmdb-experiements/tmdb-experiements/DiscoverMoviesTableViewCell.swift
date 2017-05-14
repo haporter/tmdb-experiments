250889f7672e738c3c6936d062dd75da50584a04
Andrew Porter <haporter@me.com>
2017-05-14 14:27:43 -0600

    xcode project w/ wireframes

---
+++ tmdb-experiements/tmdb-experiements/DiscoverMoviesTableViewCell.swift
@@ -0,0 +1,41 @@
+//
+//  DiscoverMoviesTableViewCell.swift
+//  tmdb-experiements
+//
+//  Created by Andrew Porter on 5/14/17.
+//  Copyright © 2017 SwiftTech. All rights reserved.
+//
+
+import UIKit
+
+class DiscoverMoviesTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+}
+
+extension DiscoverMoviesTableViewCell: UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MoviesCollectionCell", for: indexPath) as? MoviesCollectionViewCell else { return UICollectionViewCell() }
+        
+        return cell
+    }
+}
+
+extension DiscoverMoviesTableViewCell: UICollectionViewDelegate {
+    
+}
---
+++ tmdb-experiements/tmdb-experiements/MoviesCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MoviesCollectionViewCell.swift
+//  tmdb-experiements
+//
+//  Created by Andrew Porter on 5/14/17.
+//  Copyright © 2017 SwiftTech. All rights reserved.
+//
+
+import UIKit
+
+class MoviesCollectionViewCell: UICollectionViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    
+}
